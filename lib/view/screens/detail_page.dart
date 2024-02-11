32651083f845caa2af0251f43d690a721bfcfaf9
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2024-02-11 18:06:06 +0530

    10/02/2024

--- lib/view/screens/detail_page.dart
+++ lib/view/screens/detail_page.dart
@@ -30,7 +30,7 @@ class _DetailPageState extends State<DetailPage> {
             Navigator.of(context).pop();
           },
           icon: const Icon(Icons.arrow_back_sharp),
-          color: Color(0xffDDE6ED),
+          color: const Color(0xffDDE6ED),
         ),
         backgroundColor: const Color(0xff27374D),
       ),
@@ -38,20 +38,113 @@ class _DetailPageState extends State<DetailPage> {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(3, 3), blurRadius: 5, color: Colors.grey)
-                ],
-                image: DecorationImage(
-                    image: NetworkImage(
-                      data['previewURL'],
-                    ),
-                    fit: BoxFit.fill)),
-            height: h * 0.6,
-            width: double.infinity,
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                          offset: Offset(3, 3),
+                          blurRadius: 5,
+                          color: Colors.grey)
+                    ],
+                    image: DecorationImage(
+                        image: NetworkImage(
+                          data['previewURL'],
+                        ),
+                        fit: BoxFit.fill)),
+                height: h * 0.6,
+                width: double.infinity,
+              ),
+              const SizedBox(
+                height: 15,
+                width: double.infinity,
+              ),
+              Container(
+                height: h * 0.38,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Color(0xff9DB2BF),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(3, 3),
+                        blurRadius: 5,
+                        color: Colors.grey,
+                      )
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          const Text(
+                            "Tags: ",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            data['tags'],
+                            maxLines: 1,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Width : ${data['previewWidth']}",
+                      ),
+                      Text(
+                        "Height : ${data['previewHeight']}",
+                      ),
+                      Text(
+                        "webformatWidth : ${data['webformatWidth']}",
+                      ),
+                      Text(
+                        "webformatHeight : ${data['webformatHeight']}",
+                      ),
+                      Text(
+                        "imageWidth : ${data['imageWidth']}",
+                      ),
+                      Text(
+                        "imageHeight : ${data['imageHeight']}",
+                      ),
+                      Text(
+                        "imageSize : ${data['imageSize']}",
+                      ),
+                      Text(
+                        "views : ${data['views']}",
+                      ),
+                      Text(
+                        "downloads : ${data['downloads']}",
+                      ),
+                      Text(
+                        "collections : ${data['collections']}",
+                      ),
+                      Text(
+                        "likes : ${data['likes']}",
+                      ),
+                      Text(
+                        "comments : ${data['comments']}",
+                      ),
+                      Text(
+                        "user_id : ${data['user_id']}",
+                      ),
+                      Text(
+                        "user : ${data['user']}",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
